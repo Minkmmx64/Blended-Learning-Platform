d8d03b0a50abe658b1ca1ad51796967ce6eeb91f
mojiawei69 <67847786+mojiawei69@users.noreply.github.com>
2024-01-15 21:31:45 +0800

    重新添加

--- mm_stu_back/src/Request/index.type.ts
+++
@@ -1,4 +0,0 @@
-export enum Gender {
-  nan = '男',
-  nv = '女'
-}
--- mm_stu_serve/src/Modules/app/user/user.valid.ts
+++
@@ -1,7 +0,0 @@
-import * as Joi from "joi";
-import { RegisterData } from "./user.dto";
-import { NotAcceptableException } from "@nestjs/common";
-
-export const RegisterDataValid = Joi.object<RegisterData>({
-  username: Joi.string().required().error(new NotAcceptableException("用户名不能为空"))
-})
